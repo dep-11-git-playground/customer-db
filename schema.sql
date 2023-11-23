9838811a422e226036278c76f75418bf47b7b8e7
Kavindu95 <kavindueng95@gmail.com>
2023-11-23 17:15:05 +0530

    name-list

--- schema.sql
+++ schema.sql
@@ -6,4 +6,14 @@ CREATE TABLE IF NOT EXISTS customer(
     country VARCHAR(50) NOT NULL
 );
 
-INSERT INTO customer (first_name, last_name, contact, country) VALUES ('Kasun', 'Sampath', '077-1234567', 'Sri Lanka');
+INSERT INTO customer (first_name, last_name, contact, country) VALUES 
+('Kasun', 'Sampath', '077-1234567', 'Australia'),
+('Kavindu', 'Dilshan', '077-1234567', 'Sri Lanka'),
+('Nadun', 'Silva', '071-1234567', 'France'),
+('Janaka', 'Perera', '076-1234567', 'Sri Lanka'),
+('Ishan', 'Lahiru', '073-1234567', 'Italy'),
+('Thisara', 'Alwis', '074-1234567', 'German'),
+('Achinda', 'Mihiruk', '078-1234567', 'Sri Lanka'),
+('Saman', 'Gamage', '072-1234567', 'Russia'),
+('Yohan', 'Mendis', '070-1234567', 'UK'),
+;
