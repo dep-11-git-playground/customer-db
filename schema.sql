ddb7288fad68517b40d6de920b428f0cceb77d15
Ajan Eshwara <ajan.eshwara@gmail.com>
2023-11-23 17:26:31 +0530

    updated

--- schema.sql
+++ schema.sql
@@ -9,7 +9,7 @@ CREATE TABLE IF NOT EXISTS customer(
 INSERT INTO customer (first_name, last_name, contact, country) VALUES ('Kasun', 'Saman', '077-1234563', 'Dakunu rata');
 INSERT INTO customer (first_name, last_name, contact, country) VALUES ('Kamal', 'Sampun', '077-1234563', 'India');
 INSERT INTO customer (first_name, last_name, contact, country) VALUES ('Nadun', 'Sampathii', '077-1234567', 'Sri Lanka');
-INSERT INTO customer (first_name, last_name, contact, country) VALUES ('Sunimal', 'Sam', '077-1234567', 'Balukisthan);
+INSERT INTO customer (first_name, last_name, contact, country) VALUES ('Sunimal', 'Sam', '077-1234567', 'Balukisthan');
 INSERT INTO customer (first_name, last_name, contact, country) VALUES ('Kakuli', 'Sampa', '077-1234567', 'male');
 INSERT INTO customer (first_name, last_name, contact, country) VALUES ('Kira', 'Sampath2', '077-1234567', 'aussi');
 INSERT INTO customer (first_name, last_name, contact, country) VALUES ('Kanban', 'Sanath', '077-1234567', 'New Zealand');
