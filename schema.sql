8e90f15160f6d5ed89abc9d33120f3ad670c51d0
Wishva98 <buddhiwishvanath123@gmail.com>
2023-11-23 17:12:46 +0530

    add 10 customers

--- schema.sql
+++ schema.sql
@@ -6,4 +6,15 @@ CREATE TABLE IF NOT EXISTS customer(
     country VARCHAR(50) NOT NULL
 );
 
-INSERT INTO customer (first_name, last_name, contact, country) VALUES ('Kasun', 'Sampath', '077-1234567', 'Sri Lanka');
+INSERT INTO customer (first_name, last_name, contact, country) VALUES 
+('Kasun', 'Sampath', '077-1234567', 'Sri Lanka'),
+('Nimal', 'Silva', '074-1234567', 'Sri Lanka'),
+('Kamala', 'Rajapaksa', '078-2345678', 'India'),
+('Rohan', 'Fernando', ,'077-3456789', 'Australia'),
+('Anusha', 'Perera', '071-4567890', 'United States'),
+('Dilshan', 'Wickramasinghe', '070-5678901', 'Canada'),
+('Ishani', 'Jayawardena', '076-6789012', 'United Kingdom'),
+('Tharindu', 'Gunasekara', '077-7890123', 'Japan'),
+('Malini', 'De Silva', '074-8901234', 'Germany'),
+('Chaminda', 'Bandara', '072-9012345', 'France'),
+('Shalini', 'Rathnayake', '075-0123456', 'Brazil');
