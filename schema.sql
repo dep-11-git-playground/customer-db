3a1c474026ce5f788c7361195046c93fb0d652e9
Wishva98 <buddhiwishvanath123@gmail.com>
2023-11-23 17:30:39 +0530

    resolve

--- schema.sql
+++ schema.sql
@@ -6,15 +6,14 @@ CREATE TABLE IF NOT EXISTS customer(
     country VARCHAR(50) NOT NULL
 );
 
-INSERT INTO customer (first_name, last_name, contact, country) VALUES 
-('Kasun', 'Sampath', '077-1234567', 'Sri Lanka'),
-('Nimal', 'Silva', '074-1234567', 'Sri Lanka'),
-('Kamala', 'Rajapaksa', '078-2345678', 'India'),
-('Rohan', 'Fernando', ,'077-3456789', 'Australia'),
-('Anusha', 'Perera', '071-4567890', 'United States'),
-('Dilshan', 'Wickramasinghe', '070-5678901', 'Canada'),
-('Ishani', 'Jayawardena', '076-6789012', 'United Kingdom'),
-('Tharindu', 'Gunasekara', '077-7890123', 'Japan'),
-('Malini', 'De Silva', '074-8901234', 'Germany'),
-('Chaminda', 'Bandara', '072-9012345', 'France'),
-('Shalini', 'Rathnayake', '075-0123456', 'Brazil');
+INSERT INTO customer (first_name, last_name, contact, country) VALUES ('Kasun', 'Sampath', '077-1234567', 'Sri Lanka');
+INSERT INTO customer (first_name, last_name, contact, country) VALUES('Nimal', 'Silva', '074-1234567', 'Sri Lanka');
+INSERT INTO customer (first_name, last_name, contact, country) VALUES('Kamala', 'Rajapaksa', '078-2345678', 'India');
+INSERT INTO customer (first_name, last_name, contact, country) VALUES('Rohan', 'Fernando', ,'077-3456789', 'Australia');
+INSERT INTO customer (first_name, last_name, contact, country) VALUES('Anusha', 'Perera', '071-4567890', 'United States');
+INSERT INTO customer (first_name, last_name, contact, country) VALUES('Dilshan', 'Wickramasinghe', '070-5678901', 'Canada');
+INSERT INTO customer (first_name, last_name, contact, country) VALUES('Ishani', 'Jayawardena', '076-6789012', 'United Kingdom');
+INSERT INTO customer (first_name, last_name, contact, country) VALUES('Tharindu', 'Gunasekara', '077-7890123', 'Japan');
+INSERT INTO customer (first_name, last_name, contact, country) VALUES('Malini', 'De Silva', '074-8901234', 'Germany');
+INSERT INTO customer (first_name, last_name, contact, country) VALUES('Chaminda', 'Bandara', '072-9012345', 'France');
+INSERT INTO customer (first_name, last_name, contact, country) VALUES('Shalini', 'Rathnayake', '075-0123456', 'Brazil');
