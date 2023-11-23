93c6c90fe192e6ed17f1fc20409fbae500633d22
Tharindu Madushan <tharindumpathirana96@gmail.com>
2023-11-23 17:10:16 +0530

    Add my details

--- schema.sql
+++ schema.sql
@@ -6,4 +6,20 @@ CREATE TABLE IF NOT EXISTS customer(
     country VARCHAR(50) NOT NULL
 );
 
-INSERT INTO customer (first_name, last_name, contact, country) VALUES ('Kasun', 'Sampath', '077-1234567', 'Sri Lanka');
+INSERT INTO customer (first_name, last_name, contact, country) 
+VALUES 
+  ('John', 'Doe', '123-456-7890', 'USA'),
+  ('Alice', 'Smith', '987-654-3210', 'Canada'),
+  ('Mohan', 'Raj', '876-543-2109', 'India'),
+  ('Sophie', 'Brown', '234-567-8901', 'UK'),
+  ('Carlos', 'Rodriguez', '345-678-9012', 'Spain'),
+  ('Yuki', 'Tanaka', '456-789-0123', 'Japan'),
+  ('Maria', 'Gomez', '567-890-1234', 'Mexico'),
+  ('Ahmed', 'Ali', '678-901-2345', 'Egypt'),
+  ('Anna', 'Larsson', '789-012-3456', 'Sweden'),
+  ('Ravi', 'Kumar', '890-123-4567', 'India'),
+  ('Isabella', 'Fernandez', '901-234-5678', 'Spain'),
+  ('Chen', 'Wei', '012-345-6789', 'China'),
+  ('Luis', 'Martinez', '345-678-9012', 'Mexico'),
+  ('Sara', 'Andersen', '567-890-1234', 'Denmark'),
+  ('Raj', 'Patel', '789-012-3456', 'India');
