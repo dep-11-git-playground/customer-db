a9f74e7110915e4d7e81d7f0894c875e15766a4f
Ajan Eshwara <ajan.eshwara@gmail.com>
2023-11-23 17:13:27 +0530

    Add customers

--- schema.sql
+++ schema.sql
@@ -6,4 +6,14 @@ CREATE TABLE IF NOT EXISTS customer(
     country VARCHAR(50) NOT NULL
 );
 
-INSERT INTO customer (first_name, last_name, contact, country) VALUES ('Kasun', 'Sampath', '077-1234567', 'Sri Lanka');
+INSERT INTO customer (first_name, last_name, contact, country) VALUES ('Kasun', 'Saman', '077-1234563', 'Dakunu rata');
+INSERT INTO customer (first_name, last_name, contact, country) VALUES ('Kamal', 'Sampun', '077-1234563', 'India');
+INSERT INTO customer (first_name, last_name, contact, country) VALUES ('Nadun', 'Sampathii', '077-1234567', 'Sri Lanka');
+INSERT INTO customer (first_name, last_name, contact, country) VALUES ('Sunimal', 'Sam', '077-1234567', 'Balukisthan);
+INSERT INTO customer (first_name, last_name, contact, country) VALUES ('Kakuli', 'Sampa', '077-1234567', 'male');
+INSERT INTO customer (first_name, last_name, contact, country) VALUES ('Kira', 'Sampath2', '077-1234567', 'aussi');
+INSERT INTO customer (first_name, last_name, contact, country) VALUES ('Kanban', 'Sanath', '077-1234567', 'New Zealand');
+INSERT INTO customer (first_name, last_name, contact, country) VALUES ('Kaweri', 'Samanmal', '077-1234567', 'Netherland');
+INSERT INTO customer (first_name, last_name, contact, country) VALUES ('Karuna', 'Amman', '077-1234567', 'Australia');
+INSERT INTO customer (first_name, last_name, contact, country) VALUES ('Kavindu', 'Srimal', '077-1234567', 'Netherland');
+
